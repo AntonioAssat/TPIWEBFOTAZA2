e57c96b1129833a4cfc3deedcbcb759b272b532a
AntonioAssat <turcotomas280302@gmail.com>
2026-06-03 19:05:36 -0300

    Forzar carga pg

--- config/database.js
+++ config/database.js
@@ -1,3 +1,6 @@
+import pg from "pg";
+console.log("PG CARGADO:", typeof pg);
+
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 
@@ -7,6 +10,7 @@ const sequelize = new Sequelize(
     process.env.DATABASE_URL,
     {
         dialect: "postgres",
+        dialectModule: pg,
         dialectOptions: {
             ssl: {
                 require: true,
